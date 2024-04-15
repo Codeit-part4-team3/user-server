6be631b99a35b5d190ed4af22d2db81ab29f03bf
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-04-15 14:21:17 +0900

    feat : 일부 기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' })],
+  controllers: [AuthController, UserController],
+  providers: [AuthService, PrismaService, UserService],
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    return this.userService.getUser({ id });
+  }
+}
