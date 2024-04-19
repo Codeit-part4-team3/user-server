bef452375286435f8add2b6717bc5303ef0e8433
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-04-19 18:30:59 +0900

    chore : app. controller, service 복구

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { PrismaService } from './prisma.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
   ],
-  controllers: [UserController],
-  providers: [PrismaService, UserService],
+  controllers: [UserController, AppController],
+  providers: [PrismaService, UserService, AppService],
 })
 export class AppModule {}
