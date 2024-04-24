2da2b2327dea9a7f022540081c7cc5687f122061
Jnnsu <jspp120@gmail.com>
2024-04-25 03:33:11 +0900

    Fix

--- src/common/logger/logger.config.ts
+++ src/common/logger/logger.config.ts
@@ -19,17 +19,16 @@ export const dailyOptions = (level: string) => {
     datePattern: 'YYYY-MM-DD', // 날짜 포맷
     dirname: `${appRoot.path}/logs` + `/${level}`, // 저장할 URL: 여기서는  루트에 logs라는 폴더가 생기고 그 아래에 level 폴더
     filename: `%DATE%.${level}.log`,
-    maxFiles: 20, // 20일의 로그 저장
+    maxFiles: 20, // 보관할 최대 로그 수
     zippedArchive: true, // 로그가 쌓였을 때 압축
     colorize: true, // 로그 메세지에 색상 추가
     handleExceptions: true, // 로거가 애플리케이션에서 발생하는 예외를 자동으로 감지해 기록
     json: false, // JSON 형식이 아닌 일반 텍스트 형식으로 출력
 
     format: winston.format.combine(
-      appendTimestamp({ tz: 'Asia/Seoul' }), // 로그 서울 시간대를 기준으로 기록
-      winston.format.json(),
+      appendTimestamp({ tz: 'Asia/Seoul' }),
       winston.format.printf((info) => {
-        return `${info.timestamp} - ${info.level} ${info.message}`;
+        return `${info.timestamp} - ${info.level} - [Status: ${info.status}] ${info.message}`;
       }),
     ),
   };
