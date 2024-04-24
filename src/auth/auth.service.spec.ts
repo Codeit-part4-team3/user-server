4f42f9d52b8f5d1fd383153866045c4467ac185d
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-04-24 16:43:31 +0900

    test : 누락된 service 추가

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma.service';
+import { UserService } from '../user/user.service';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -9,7 +10,7 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule],
-      providers: [AuthService, PrismaService],
+      providers: [AuthService, PrismaService, UserService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
