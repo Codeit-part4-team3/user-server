a8f7541996c76ce148752b73e429d54d6d66f255
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-04-26 06:20:19 +0900

    test : 테스트 오류 수정

--- src/friend/friend.controller.spec.ts
+++ src/friend/friend.controller.spec.ts
@@ -3,14 +3,17 @@ import { FriendController } from './friend.controller';
 import { PrismaService } from './../prisma.service';
 import { FriendService } from './friend.service';
 import { UserService } from '../user/user.service';
+import { AuthService } from '../auth/auth.service';
+import { LoggerModule } from './../common/logger/logger.module';
 
 describe('FriendController', () => {
   let controller: FriendController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [LoggerModule],
       controllers: [FriendController],
-      providers: [PrismaService, FriendService, UserService],
+      providers: [PrismaService, FriendService, UserService, AuthService],
     }).compile();
 
     controller = module.get<FriendController>(FriendController);
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,14 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from './../prisma.service';
+import { AuthService } from './../auth/auth.service';
+import { LoggerModule } from './../common/logger/logger.module';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [LoggerModule],
       controllers: [UserController],
-      providers: [UserService, PrismaService],
+      providers: [UserService, PrismaService, AuthService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
