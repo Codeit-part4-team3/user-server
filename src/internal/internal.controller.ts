9d2bfdfbe3488f48c830f6459ccd1664e4d08568
ohddang <ohddang509@gmail.com>
2024-05-04 20:43:44 +0900

    feat: 이메일 초대 db 업데이트 완료

--- src/internal/internal.controller.ts
+++ src/internal/internal.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { InternalService } from './internal.service';
 import { VerifyEmailDto } from 'src/dto/internal.dto';
+import { User } from '@prisma/client';
 
 @Controller('internal/v1')
 export class InternalController {
@@ -8,7 +9,7 @@ export class InternalController {
 
   @Post('verifyEmail')
   @HttpCode(200)
-  async verifyEmail(@Body() verifyEmailDto: VerifyEmailDto): Promise<string> {
+  async verifyEmail(@Body() verifyEmailDto: VerifyEmailDto): Promise<User> {
     return this.internalService.verifyEmail(verifyEmailDto.email);
   }
 }
--- src/internal/internal.service.ts
+++ src/internal/internal.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class InternalService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async verifyEmail(email: string): Promise<string> {
-    return `Email ${email} is verified`;
+  async verifyEmail(email: string): Promise<User> {
+    return this.prismaService.user.findUnique({
+      where: { email: email },
+    });
   }
 }
